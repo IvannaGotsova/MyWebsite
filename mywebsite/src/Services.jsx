80c778c5a4f88c7b4d3ed3ea3a5af11c7fee7620
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-10 18:46:32 +0300

    Added className service-item

--- mywebsite/src/Services.jsx
+++ mywebsite/src/Services.jsx
@@ -7,7 +7,7 @@ function Services () {
         <div>
             <div className="service-box">
             {db.services.map((service) => (
-                <div key={service.id}>
+                <div className="service-item" key={service.id}>
                 <h2>{service.type}</h2>
                 <img src={service.photo} alt="" />
                 <p>{service.description}</p>
