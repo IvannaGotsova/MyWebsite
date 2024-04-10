a9872bd4303fdf42571be632aaf80d585c035169
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-10 18:39:16 +0300

    Added Header and Footer

--- mywebsite/src/App.js
+++ mywebsite/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Services from './Services';
+import Header from './Header';
+import Footer from './Footer';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Services />
+      <Footer />
     </div>
   );
 }
