bcbc1e9148272bce3dcb1c6435533b0c6d9a72e3
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-10 18:34:57 +0300

    Updated under title

--- mywebsite/src/Services.jsx
+++ mywebsite/src/Services.jsx
@@ -5,15 +5,20 @@ function Services () {
 
     return (
         <div>
-            <h1>Services</h1>
+            <h1>SERVICES</h1>
+            <h3>The place where we can help you</h3>
+            <hr />
+            <div className="service-box">
             {db.services.map((service) => (
                 <div key={service.id}>
                 <h2>{service.type}</h2>
+                <img src={service.photo} alt="" />
                 <p>{service.description}</p>
                 <p>{service.time}</p>
                 <p>{service.price}</p>
                 </div>
       ))}
+            </div>
     </div>
     )
 }
