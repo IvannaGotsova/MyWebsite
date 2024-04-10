58a086bfd455338f74df599d70af0308a3b631fe
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-10 21:52:26 +0300

    Added form tag with label and input for search

--- mywebsite/src/Header.jsx
+++ mywebsite/src/Header.jsx
@@ -5,6 +5,11 @@ function Header() {
         <div>
             <h1>SERVICES</h1>
             <h3>The place where we can help you</h3>
+            <form action="">
+                <label for="">Search</label>
+                <input type="search" name="" id="" placeholder="I am looking for..." />
+                <input type="submit" value="Submit"></input>
+            </form>
             <hr />
         </div>
     )
