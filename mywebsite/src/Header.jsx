df2c09fb11b4081b01982bdda0bb7528dc14bd57
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-10 18:40:06 +0300

    Created basic Header

---
+++ mywebsite/src/Header.jsx
@@ -0,0 +1,13 @@
+
+
+function Header() {
+    return (
+        <div>
+            <h1>SERVICES</h1>
+            <h3>The place where we can help you</h3>
+            <hr />
+        </div>
+    )
+}
+
+export default Header 
