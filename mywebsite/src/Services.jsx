71db255f18976d2f3fcf700c8bae8ed7fe7e3693
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-12 22:24:56 +0300

    Added two buttons for every service

--- mywebsite/src/Services.jsx
+++ mywebsite/src/Services.jsx
@@ -1,24 +1,32 @@
-import React from "react"
-import db from "./db.json"
-import Picture from './Picture.jpg';
+import React from "react";
+import db from "./db.json";
 
-function Services () {
 
-    return (
-        <div>
-            <div className="service-box">
-            {db.services.map((service) => (
-                <div className="service-item" key={service.id}>
-                <h2>{service.type}</h2>
-                <img className="service-image" src={require(`${service.photo}`)} alt="" />
-                <p>{service.description}</p>
-                <p>Duration: {service.time} minutes</p>
-                <p>Price: {service.price} USD</p>
-                </div>
-      ))}
+function Services() {
+
+  return (
+    <div>
+      <div className="service-box">
+        {db.services.map((service) => (
+            <div className="service-item" key={service.id}>
+              <h2>{service.type}</h2>
+              <img
+                className="service-image"
+                src={require(`${service.photo}`)}
+                alt=""
+              />
+              <p>{service.description}</p>
+              <p>Duration: {service.time} minutes</p>
+              <p>Price: {service.price} USD</p>
+              <button onclick="myViewMoreButtonFunction()">View more</button>
+              <br />
+              <button onclick="myMakeAnAppointmentButtonFunction()">Make an appointment</button>
+              <br />
             </div>
+          ))}
+      </div>
     </div>
-    )
+  );
 }
 
-export default Services
+export default Services;
