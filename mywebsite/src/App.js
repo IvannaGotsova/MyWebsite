fac6f3bc37585a43b498e9b3e2a0a1ed4bc9fcfa
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-12 23:32:07 +0300

    Added BrowserRouter, Route, Routes

--- mywebsite/src/App.js
+++ mywebsite/src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
-import Services from './Services';
-import Header from './Header';
-import Footer from './Footer';
-
+import "./App.css";
+import Services from "./Services";
+import Header from "./Header";
+import Footer from "./Footer";
+import ViewMore from "./ViewMore";
+import MakeAnAppointment from "./MakeAnAppointment";
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/viewmore" component={ViewMore} />
+        <Route path="/makeanappointment" component={MakeAnAppointment} />
+      </Routes>
+    </BrowserRouter>,
+    rootElement
+  );
+
   return (
+    <BrowserRouter>
     <div className="App">
       <Header />
       <Services />
@@ -14,6 +29,7 @@ function App() {
       <Services />
       <Footer />
     </div>
+    </BrowserRouter>
   );
 }
 
