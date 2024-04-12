02a6a25c920045f0d2db79ce4b2763e9d5b11653
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-12 23:00:25 +0300

    Added new component

---
+++ mywebsite/src/MakeAnAppointment.jsx
@@ -0,0 +1,15 @@
+
+
+function MakeAnAppointment () {
+    <form action="">
+        <input type="text" name="" id="" placeholder="First Name ..."/>
+        <input type="text" name="" id="" placeholder="Last Name ..."/>
+        <input type="text" name="" id="" placeholder="How we can help you ..."/>
+        <input type="datetime-local" name="" id="" />
+        <input type="number" name="" id="" placeholder="Phone Number"/>
+        <input type="email" name="" id="" placeholder="Email"/>
+        <button type="submit"></button>
+    </form>
+}
+
+export default MakeAnAppointment
