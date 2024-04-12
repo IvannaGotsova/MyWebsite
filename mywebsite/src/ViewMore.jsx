7850c224798b721b802eb4c4489429b02776967b
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-12 23:00:35 +0300

    Added new component

---
+++ mywebsite/src/ViewMore.jsx
@@ -0,0 +1,16 @@
+
+
+function ViewMore () {
+
+    <div>
+        <h4>Details</h4>
+        <p>Location: Online, Live</p>
+        <p>Workdays: Monday - Saturday</p>
+        <p>More about us</p>
+        <p>Contacts: Ivan Ivanov</p>
+        <p>Phone Number: 0000000000</p>
+        <p>Email: aaa@aaa.com</p>
+    </div>
+}
+
+export default ViewMore
