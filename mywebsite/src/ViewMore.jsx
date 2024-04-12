04fc27b665bd4e9d8364a64045ea4842b5d8ae83
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-12 23:14:15 +0300

    Added address in the component

--- mywebsite/src/ViewMore.jsx
+++ mywebsite/src/ViewMore.jsx
@@ -8,6 +8,7 @@ function ViewMore () {
         <p>Workdays: Monday - Saturday</p>
         <p>More about us</p>
         <p>Contacts: Ivan Ivanov</p>
+        <p>Address: Aaaaaaa</p>
         <p>Phone Number: 0000000000</p>
         <p>Email: aaa@aaa.com</p>
     </div>
