4d40b715be329826ab72a162b11987e86042373a
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-12 22:25:17 +0300

    Remove Submit button in Header

--- mywebsite/src/Header.jsx
+++ mywebsite/src/Header.jsx
@@ -1,6 +1,6 @@
+import Services from "./Services"
 
-
-function Header() {
+function Header () {
     return (
         <div>
             <h1>SERVICES</h1>
@@ -8,7 +8,6 @@ function Header() {
             <form action="">
                 <label for="">Search</label>
                 <input type="search" name="" id="" placeholder="I am looking for..." />
-                <input type="submit" value="Submit"></input>
             </form>
             <hr />
         </div>
